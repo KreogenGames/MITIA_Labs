b3f135da01406375b4a8ffa8d837e44ca1a93537
Kreogen <maris.sibgatulov@gmail.com>
2024-12-19 13:04:42 +0300

    Инициализирующий коммит с заготовкой проекта

---
+++ src/main/java/ru/mirea/mitia/MitiaApplication.java
@@ -0,0 +1,11 @@
+package ru.mirea.mitia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitiaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MitiaApplication.class, args);
+    }
+}
